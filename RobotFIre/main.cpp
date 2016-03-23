b844766212317023ed4496d7ea96b13de464671e
aivrit <a.ivrit@gmail.com>
2016-03-23 13:20:10 -0700

    hi

--- RobotFIre/main.cpp
+++ RobotFIre/main.cpp
@@ -16,20 +16,65 @@ int main() {
 	Position2dProxy pp(&pc);
 	SonarProxy sp(&pc);
 
+	double scans[5];
 	//pp.SetSpeed(0.5, 0);
 	pp.SetOdometry(-6.009, 2.481, dtor(-194.220));
 
+	for (int i=0; i<5; i++)
+	{
+		scans[i] = 100;
+	}
+
 	while (true) {
 		pc.Read();
-		std::cout << "X: " << pp.GetXPos() << ", Y: " << pp.GetYPos()
-				<< ", Yaw: " << pp.GetYaw();
 
-		std::cout << sp << std::endl;
+		std::cout << "array: [";
+		for(int i=0; i<5;  i++)
+		{
+			std::cout << scans[i] << ', ' ;
+		}
 
-		if (sp[2] < 0.5)
-			pp.SetSpeed(0, 0.5);
-		else
-			pp.SetSpeed(0.5, 0);
+		std::cout << " ] " << endl;
+
+
+		for (int i=0; i<5; i++)
+		{
+			scans[i] = sp[i];
+		}
+			if (scans[1] < 0.3 || scans[3] < 0.3)
+			{
+				if (scans[3] < 0.3 && scans[1] < 0.3)
+				{
+					if (scans[0] > scans[4])
+					{
+						std::cout << " went for going no choice right" << endl;
+						pp.SetSpeed(0, -3.14);
+
+					}
+					else
+					{
+						std::cout << " went for going no choice left" << endl;
+						pp.SetSpeed(0, 3.14);
+
+
+					}
+				}
+				else if (scans[3] < 0.3)
+				{
+					std::cout << " went for going  right" << endl;
+					pp.SetSpeed(0, -0.6);
+				}
+				else
+				{
+					std::cout << " went for going left " << endl;
+					pp.SetSpeed(0, 0.6);
+				}
+			}
+			else
+			{
+				std::cout << " went for going straight" << endl;
+					pp.SetSpeed(0.2, 0);
+			}
 	}
 
 }
