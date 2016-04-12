570f234e98f0390dbd6a5b9874ebcb57beb91c2b
aivrit <a.ivrit@gmail.com>
2016-04-12 10:57:15 -0700

    hi

---
+++ RobotFIre/targil.cpp
@@ -0,0 +1,14 @@
+#include <libplayerc++/playerc++.h>
+#include <iostream>
+#include <cmath>
+
+using namespace PlayerCc;
+using namespace std;
+
+// second Milestone - Robotics Class - Michael lefkovits 204206445 and Alon Bar Tzlil 302276126
+int main() {
+
+
+	double scans[5];
+
+}
